2529f011d0126ec72132126bc059322905be9b3c
EmJ <emily@example.com>
2022-07-29 11:27:47 -0700

    got number of doctors data to appear in chart

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //import DataFrame from 'pandas-js';
-
+//import Chart from "chart.js/auto"
 
 // SWITCH STUFF HERE //
 //var employeeLabel = [], employeeSalaryData = [], employeeAgeData = []
-var dateData = [], doctorData = []
+var dateData = [], appointmentData = [], doctorData = []
 // , nurseData = [], maintenanceData = []
 
 async function dummyChart() {
@@ -52,30 +52,68 @@ const ctx = document.getElementById('myChart').getContext('2d');
 
 const chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'bar',
+    type: 'line',
 
     // The data for our dataset
     data: {
         labels: dateData,
         datasets: [{
-            label: 'Patient',
-            backgroundColor: 'blue',
-            borderColor: 'rgb(255, 99, 132)',
-            data: doctorData
+            label: 'Daily appointments',
+            fill: false,
+            backgroundColor: 'rgb(75, 192, 192)',
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.05,
+            data: appointmentData,
         },
+        {
+          label: 'Number doctors',
+          fill: false,
+          backgroundColor: 'rgb(255, 204, 153)',
+          borderColor: 'rgb(255, 204, 153)',
+          tension: 0.05,
+          data: doctorData,
+      },
       ]
     },
 
     // Configuration options go here
     options: {
-      tooltips: {
-        mode: 'index'
+      curve: false,
+      responsive: true,
+      legend: {
+        position: 'bottom',
+      },
+      scales: {
+          yAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Number of appointments',
+              },
+              ticks: {
+                  beginAtZero: true,
+                  steps: 5,
+                  stepValue: 2,
+              }
+          }],
+          xAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Date',
+            },
+            ticks: {
+                beginAtZero: true,
+                steps: 5,
+                stepValue: 2,
+            }
+        }]
       }
-    }
+  }
 })
 
 console.log("chartdate: ", dateData)
-console.log("patient: ", doctorData)
+console.log("patient: ", appointmentData)
 }
 
 
@@ -100,29 +138,29 @@ async function getDummyData() {
 }
 
 async function getPatientTrafficData() {
-  //const [room, setRoom] = useState([])
 
   // const getRoomData = async () => {
     const response = await fetch("http://localhost:8020/account/users/1");
     const data = await response.json()
-    console.log(data);
 
     const roomResponse = await fetch(`http://localhost:8020/internal/urgentcare/${data.pk}/rooms/`);
     const roomData = await roomResponse.json();
-    console.log("room data: ", roomData);
-    //setRoom(roomData)
 
     const date = roomData.map((x) => x.date)
-    // const list_of_tuples = roomData.map((x) => ({
-    //       date: x.date,
-    //       doctor: x.doctor
-    //     }));
-    const list_of_tuples = roomData.map(function(item) {
-      return {
-        date: item.date,
-        doctor: item.doctor
-      };
-    });
+    const list_of_tuples = roomData.map((x) => ({
+          date: x.date,
+          doctor: x.doctor
+        }));
+    // another way to use a map function
+    // const list_of_tuples = roomData.map(function(item) {
+    //   return {
+    //     date: item.date,
+    //     doctor: item.doctor,
+    //     patient: item.patientVO,
+    //   };
+    // });
+
+    // store number of appointments/day in object d
     let d = {}
     for (let i in list_of_tuples) {
       let item = list_of_tuples[i]
@@ -133,24 +171,38 @@ async function getPatientTrafficData() {
         d[itemdate] = 1
       }
     }
+
+    // store number of unique doctor pks/day in object p
+    let p = {}
+    for (let i in list_of_tuples) {
+      let item = list_of_tuples[i];
+      if (item.date in p) {
+        p[item.date].add(item.doctor);
+      } else {
+        p[item.date] = new Set([item.doctor]);
+      }
+    }
+    
+
+    // convert d object to a list and save date and appointment count as 2 different constants
     const d_list = Object.entries(d);
-    console.log("DA LIST: ", d_list);
     d_list.sort(function(a,b){return a[0].localeCompare(b[0])});
-    //console.log("LIST AGAIN: ", test);
-    const chartdate = d_list.map((x) => x[0])
-    const chartpatients = d_list.map((x) => x[1])
+    const chartDate = d_list.map((x) => x[0])
+    const numberAppointments = d_list.map((x) => x[1])
 
 
-    console.log("wtf is d: ", d)
-    console.log("date: ", date);
-    //const doctor = roomData.map((x) => x.doctor)
-    //console.log("doctor: ", doctor);
-    dateData = chartdate;
-    doctorData = chartpatients;
-    console.log("chartdate: ", dateData)
-    console.log("patient: ", doctorData)
-  // }
-  // getRoomData();
+    // convert p object to a list and save doctor count as a constant
+    const doctorCount = Object.entries(p)
+    doctorCount.sort(function(a,b){return a[0].localeCompare(b[0])});
+    // commenting out saving date for doctor
+    // const doctor_labels = doctorCount.map((x) => x[0])
+    // console.log("doctor_labels: ", doctor_labels);
+    const unqiueDailyDoctors = doctorCount.map((x) => (x[1].size))
+
+    // save constants to vars for chart
+    dateData = chartDate;
+    appointmentData = numberAppointments;
+    doctorData = unqiueDailyDoctors;
 }
 
 
